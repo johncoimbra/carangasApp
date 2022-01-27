8082f6ec14e39f2465e8491fc7e598298c9968c2
Luis Eduardo Silva Oliveira <luiseduardosilvaoliveira@MBP-de-Kabum.Dlink>
2022-01-27 13:35:17 -0300

    Project Adjustment

--- Carangas/AppDelegate.swift
+++ Carangas/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Carangas
-//
-//  Created by Eric Brito on 21/10/17.
-//  Copyright © 2017 Eric Brito. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
---
+++ Carangas/REST.swift
@@ -0,0 +1,9 @@
+//
+//  REST.swift
+//  Carangas
+//
+//  Created by Luis Eduardo Silva Oliveira on 26/01/22.
+//  Copyright © 2022 Eric Brito. All rights reserved.
+//
+
+import Foundation
