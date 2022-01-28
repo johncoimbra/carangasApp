0419367ce89b6e869787c8647ea4c24868db3eb1
Luis Eduardo Silva Oliveira <luiseduardosilvaoliveira@MBP-de-Kabum.Dlink>
2022-01-27 21:40:05 -0300

    Retrieving Car Brands from the FIPE Table

--- Carangas/AddEditViewController.swift
+++ Carangas/AddEditViewController.swift
@@ -10,52 +10,115 @@ class AddEditViewController: UIViewController {
     @IBOutlet weak var btAddEdit: UIButton!
     @IBOutlet weak var loading: UIActivityIndicatorView!
     
+    
     // MARK: - Properties
-    var car: Cars!
+    var car: Car!
+    var brands: [Brand] = []
+    lazy var pickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        pickerView.backgroundColor = .white
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        return pickerView
+    }()
+    
     // MARK: - Super Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        if car != nil {
+            tfBrand.text = car.brand
+            tfName.text = car.name
+            tfPrice.text = "\(car.price)"
+            scGasType.selectedSegmentIndex = car.gasType
+            btAddEdit.setTitle("Alterar carro", for: .normal)
+        }
         
+        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 44))
+        toolbar.tintColor = UIColor(named: "main")
+        let btCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
+        let btSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let btDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        toolbar.items = [btCancel, btSpace, btDone]
+        tfBrand.inputAccessoryView = toolbar
+        tfBrand.inputView = pickerView
         
+        loadBrands()
     }
     
     // MARK: - IBActions
     @IBAction func addEdit(_ sender: UIButton) {
         
+        sender.isEnabled = false
+        sender.backgroundColor = .gray
+        sender.alpha = 0.5
+        loading.startAnimating()
+        
         if car == nil {
-            car = Cars()
+            car = Car()
         }
         
         car.name = tfName.text!
         car.brand = tfBrand.text!
         
         if (tfPrice.text?.isEmpty)!{
-                    tfPrice.text="0.0"
-                }
-
-                if let preco = tfPrice.text {
-                    car.price = Double(preco) ?? 0.0
-                }
+            tfPrice.text="0.0"
+        }
+        if let preco = tfPrice.text {
+            car.price = Double(preco) ?? 0.0
+        }
         car.gasType = scGasType.selectedSegmentIndex
         
         if car._id == nil {
             REST.save(car: self.car) { success in
                 self.goBack()
             }
+        } else {
+            REST.update(car: car) { (success) in
+                self.goBack()
+            }
         }
-        
-        
     }
     
     // MARK: - Methods
+    
+    func loadBrands() {
+        REST.loadBrands { (brands) in
+            if let brands = brands {
+                self.brands = brands.sorted(by: {$0.nome < $1.nome})
+                DispatchQueue.main.async {
+                    self.pickerView.reloadAllComponents()
+                }
+            }
+        }
+    }
+    
     func goBack() {
         DispatchQueue.main.async {
             self.navigationController?.popViewController(animated: true)
         }
-        
     }
     
-                  
+    @objc func cancel() {
+        tfBrand.resignFirstResponder()
+    }
     
+    @objc func done() {
+        tfBrand.text = brands[pickerView.selectedRow(inComponent: 0)].nome
+        cancel()
+    }
+}
 
+extension AddEditViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return brands.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        let brand = brands[row]
+        return brand.nome
+    }
 }
